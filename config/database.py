babce41c858d2c1340f513a88219386c0fc28548
[jo] <jonespjunior@gmail.com>
2025-01-28 17:58:15 -0400

    adding initial database diagram and preparing enviroment

---
+++ config/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite+aiosqlite:///./cineAPI.db"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+async def get_db():
+    async with async_session() as session:
+        yield session
